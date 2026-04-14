5f52400454e1a9ebb5b5b28fdce5c87fa44529df
Dania Kabakibi <daniakaba99@gmail.com>
2026-04-13 22:15:44 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 sakila is a database used to collect information about inventory, customer and film for use by developers
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
