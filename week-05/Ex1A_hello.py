b25f95647aca30de80b05535d3d25808f722cdb4
Dania Kabakibi <daniakaba99@gmail.com>
2026-05-04 12:26:15 -0400

    completed Exercise_1A

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,20 @@
 print('Hello world!')
+
+message = 'Hello workd!'
+print(message)
+
+# "Hello world!" prints twice because we have two print functions with 'Hello world' message
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+# I have noticed that the result 3.5 -> is float
+
+cents = cents + .25
+print(dollars + cents)
+
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + ' ' + c_str)
