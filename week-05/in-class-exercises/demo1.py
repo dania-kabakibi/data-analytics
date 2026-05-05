57d2ac178f71abff75e1a89734856186d17beda5
Dania Kabakibi <daniakaba99@gmail.com>
2026-05-04 21:36:17 -0400

    Added week-5 day1 in class exercise

---
+++ week-05/in-class-exercises/demo1.py
@@ -0,0 +1,18 @@
+# Dania Kabakibi
+# 5/4/2026
+
+'''
+name = "Dania"
+age = 27
+
+print(name)
+print(age)
+
+print(f'My name is {name} and I am {age} years old')
+'''
+
+pi = 3.145678
+print(f"The value of pi is {pi:.2f}")
+
+sale = 12345.678
+print(f"The value of sale is ${sale:,.2f}")
