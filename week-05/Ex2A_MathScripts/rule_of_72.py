d5febecce71b15d66571f07e5d3c4826d5fb9083
Dania Kabakibi <daniakaba99@gmail.com>
2026-05-06 11:43:47 -0400

    added rule_of_72

---
+++ week-05/Ex2A_MathScripts/rule_of_72.py
@@ -0,0 +1,9 @@
+interest_rate = 6  # 6%
+saving = 10200.75
+
+years_to_double = 72 / interest_rate
+doubled_saving = saving * 2
+
+print(f"Your current savings is {saving}.")
+print(f"At a {format(interest_rate / 100, '.0%')} interest rate, your savings account will be\n" 
+      f"worth {format(doubled_saving, '.2f')} in {format(years_to_double, '.1f')} years")
