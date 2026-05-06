55b8c1c9196e6052a70ad3f24428dd7b67e8e5cb
Dania Kabakibi <daniakaba99@gmail.com>
2026-05-06 12:15:37 -0400

    added c_to_f

---
+++ week-05/Ex2B_MoreMathScripts/c_to_f.py
@@ -0,0 +1,7 @@
+# convert a temperature from Celsius to Fahrenheit
+
+c_temp = float(input("Enter the temperature in Celsius: "))
+
+f_temp = (c_temp * 9/5) + 32
+
+print(f"The temperature is {f_temp:.2f}°F")
