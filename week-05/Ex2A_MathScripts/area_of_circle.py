7e145e52c15d2b33fc0e384436d2f7edf5489a09
Dania Kabakibi <daniakaba99@gmail.com>
2026-05-05 23:08:33 -0400

    added area_of_circle

---
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -0,0 +1,8 @@
+from math import pi
+
+diameter = 15 # day of birthday
+radius = diameter / 2
+
+area_of_circle = pi * (radius ** 2)
+
+print(f"The area of a circle with radius {radius} is {area_of_circle:.2f}")
