05d46d687e564188f2d7bf49fcaf178e2a758079
Dania Kabakibi <daniakaba99@gmail.com>
2026-05-05 22:43:46 -0400

    added area_of_rectangle

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,8 @@
+length = 15 # day 15th
+width = 6 # month June
+
+area_of_rectangle = length * width
+
+print(f"Side A is {length}") 
+print(f"Side B is {width}") 
+print(f"The area of the rectangle is {area_of_rectangle}")
