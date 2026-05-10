079da375e6e8cd3be873882a94be8b30db4b3845
Dania Kabakibi <daniakaba99@gmail.com>
2026-05-09 21:52:34 -0400

    added loop_love lab1

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,8 @@
+''' Lab 1 '''
+
+great_synonyms = ["excellent", "wonderful", "terrific", "awesome", "magnificent", "amazing"]
+
+for item in great_synonyms:
+    print(f"Loops are {item}!")
+
+print("I <3 loops")
